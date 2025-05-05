5b5147d2c732bfdcde7724dce8d7d9068f4488b6
codejordy25 <jordytsimba11@gmail.com>
2025-05-05 15:29:51 +0100

    Handling New Answers Section2

--- src/components/App.js
+++ src/components/App.js
@@ -13,6 +13,7 @@ const initialState = {
   status: "loading",
   index: 0,
   answer: null,
+  points: 0,
 };
 function reducer(state, action) {
   switch (action.type) {
@@ -23,7 +24,15 @@ function reducer(state, action) {
     case "start":
       return { ...state, status: "active" };
     case "newAnswer":
-      return { ...state, answer: action.payload };
+      const question = state.questions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.questins + question.points
+            : state.points,
+      };
 
     default:
       throw new Error("Unknown Action");
